1052caa9ecce2f9d7e590a99ab91ddd6d9bc4bca
devisscher <devisscher.thomas@gmail.com>
2016-10-08 15:40:55 -0400

    use pid

--- routes/index.js
+++ routes/index.js
@@ -75,13 +75,13 @@ router.get('/appstartpm', function(req, res, next) {
     });
 });
 router.get('/appstoppm', function(req, res, next) {
-    var startScript = req.query.startScript;
+    var startScript = req.query.execPath;
     pm2.connect(function(err) {
         if (err) {
             console.error(err);
             process.exit(2);
         }
-        pm2.stop({ script: startScript }, function(err, apps) {
+        pm2.stop(startScript, function(err, apps) {
             pm2.disconnect(); // Disconnect from PM2
             if (err) throw err
         });
